9d304c7881d278964d285aa6a291717e55d699af
GabFigueiredo <gabrielfandradee2@gmail.com>
2025-04-04 11:19:48 -0300

    Update Concessionária

--- Projeto_Concessionária/src/projeto_concessionária/Projeto_Concessionária.java
+++ Projeto_Concessionária/src/projeto_concessionária/Projeto_Concessionária.java
@@ -4,6 +4,12 @@
  */
 package projeto_concessionária;
 
+import projeto_concessionária.Model.FiltroBusca;
+import projeto_concessionária.Model.Veiculo;
+import projeto_concessionária.Model.enums.Status;
+import projeto_concessionária.Model.enums.TipoCombustivel;
+import projeto_concessionário.Controller.VeiculoController;
+
 /**
  *
  * @author Elise
@@ -14,7 +20,26 @@ public class Projeto_Concessionária {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Veiculo v = new Veiculo("Honda", "HRV", "GAB-1234", 2024, "Preto", TipoCombustivel.DIESEL, 74000.0, Status.DISPONIVEL, "chassi", 120000.0);
+        
+        VeiculoController VController = new VeiculoController();
+        
+        // Cadastro
+        VController.cadastrarVeiculo(v);
+        VController.ListarVeiculos();
+        
+        // Exclusão
+        VController.removerVeiculo("chassi");
+        System.out.println("Veículos cadastrados: ");
+        VController.ListarVeiculos();
+
+        // Filtragem 
+        // FiltroBusca filtro = new FiltroBusca();
+        // filtro.setMarca("Honda");
+        //VController.ListarVeiculosFiltrados(filtro);
+
     }
+    
+    
     
 }
