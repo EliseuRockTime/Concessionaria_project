f757b87f1cbd6f9ef7ca4c55224bde590feb445f
Guilherme <guilhermefloesmmarchi@gmail.com>
2025-04-04 16:09:20 -0300

    Create FuncionarioController

--- Projeto_Concessionária/src/projeto_concessionário/Controller/FuncionarioController.java
+++ Projeto_Concessionária/src/projeto_concessionário/Controller/FuncionarioController.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package projeto_concessionário.Controller;
+import java.util.List;
+import java.util.ArrayList;
+import projeto_concessionária.Model.Funcionario;
+
 
 /**
  *
  * @author Guilherme
  */
 public class FuncionarioController {
+    private List<Funcionario> f = new ArrayList<>();
     
+    public Funcionario login(String login, String senha) {
+    return f.stream()
+            .filter(func -> func.getLogin().equals(login) && func.getSenha().equals(senha))
+            .findFirst()
+            .orElse(null);
+    }
+    
+    public boolean cadastrarFuncionario(Funcionario novoFuncionario) {
+    
+    if (novoFuncionario == null) {
+        return false;
+    }
+    
+    f.add(novoFuncionario);
+    return true;
 }
+
+ 
+}    
+    
