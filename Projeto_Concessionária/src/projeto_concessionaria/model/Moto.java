7fcba4b163a32f332e968db0ea08b2c9d849a13e
Eliseu Portes <168044613+EliseuRockTime@users.noreply.github.com>
2025-04-14 20:52:25 -0300

    Update Moto.java
    Apaguei a classe moto...

--- Projeto_Concessionária/src/projeto_concessionaria/model/Moto.java
+++ Projeto_Concessionária/src/projeto_concessionaria/model/Moto.java
@@ -1,34 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package projeto_concessionaria.Model;
-
-import projeto_concessionaria.model.enums.Status;
-import projeto_concessionaria.model.enums.TipoCombustivel;
-
-/**
- *
- * @author Guilherme
- */
-public class Moto extends Veiculo {
-    private Integer quantidadeDeRodas;
-
-    public Moto(String marca, String modelo, String placa, Integer anoFabricacao, String cor, TipoCombustivel tipoCombustivel, Double quilometragem, Status status, String chassi, double preco) {
-        super(marca, modelo, placa, anoFabricacao, cor, tipoCombustivel, quilometragem, status, chassi,preco);
-        this.quantidadeDeRodas = quantidadeDeRodas;
-    }
-
-    public Integer getQuantidadeDeRodas() {
-        return quantidadeDeRodas;
-    }
-
-    public void setQuantidadeDeRodas(Integer quantidadeDeRodas) {
-        this.quantidadeDeRodas = quantidadeDeRodas;
-    }
-
-    
-
 
     
     
